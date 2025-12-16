dd3cd6da7e9cbb8b01dab6a059c6123747584c2a
Gabriela Zemencikova <gabrielazemencikova@Gabrielas-MacBook-Pro-2.local>
2025-12-16 16:31:53 +0100

    deploy-fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/drg-helper/', // Zmeňte na názov vášho GitHub repozitára
+  base: '/drg-oft/',
 })
